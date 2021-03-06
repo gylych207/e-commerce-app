161be598733480f8b60e361cc54a060029a57e08
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-06 16:34:20 -0500

    google sign in btn styled

--- src/components/customButton/CustomButton.jsx
+++ src/components/customButton/CustomButton.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import './styles.scss';
-const CustomButton = ({children,isGoogleSignIn, ...otherProps}) => {
-  return (
-    <div className={`${isGoogleSignIn ? 'google-sign-in':''}'custom-button'`} {...otherProps}>
-      {children}
-    </div>
-  );
-};
+
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 
 export default CustomButton;
--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -44,7 +44,7 @@ class SignIn extends Component {
             required />
           
           <CustomButton type='submit'>Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
 				</form>
 			</div>
 		);
