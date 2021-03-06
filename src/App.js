4cddcc1e8cbaafa89794dfdf6292958388f7d371
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-06 16:10:57 -0500

    auth subsrc and unsrbsr set up app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import './App.css';
+import React from 'react';
 import HomePage from './Screens/home/HomePage.jsx';
 import ShopPage from './Screens/shop/ShopPage.jsx';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/Header';
 import SignInSignUp from './components/signInSignUp/SignInSignUp';
+import { auth } from './components/firebase/utils';
 
 
-function App() {
-	return (
-    <div>
-      <Header/>
-			<Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInSignUp}/>
-			</Switch>
-		</div>
-	);
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
+      this.setState({ currentUser: user });
+      
+  })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInSignUp}/>
+        </Switch>
+      </div>
+    );
+  }
+	
 }
 
 export default App;
