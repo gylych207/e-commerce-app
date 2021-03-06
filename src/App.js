a3235c5aef961bd705ba26ffd58905ce79c48277
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-05 21:24:47 -0500

    sign in component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './Screens/home/HomePage.jsx';
 import ShopPage from './Screens/shop/ShopPage.jsx';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/Header';
+import SignInSignUp from './components/signInSignUp/SignInSignUp';
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Header/>
 			<Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp}/>
 			</Switch>
 		</div>
 	);
