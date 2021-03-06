8e4223f651902bff9dab0f04116ad0671e2549e5
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-06 15:19:24 -0500

    utils js with google sign in

--- src/components/firebase/utils.js
+++ src/components/firebase/utils.js
@@ -12,3 +12,12 @@ const config = {
   measurementId: "G-3MJ0L5K28T"
 };
 firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
