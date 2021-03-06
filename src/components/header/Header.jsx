e1aaa71b16be46fd0d96c529f13d907ae3b64093
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-06 16:19:46 -0500

    sign in and sign out option added to navbar with authentication

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/4.3 crown.svg';
+import { auth } from '../firebase/utils.js';
 
-const Header = () => {
+const Header = ({currentUser}) => {
 	return (
 		<div className="header">
 			<Link className="logo-container" to="/">
@@ -14,8 +15,14 @@ const Header = () => {
           SHOP
 				</Link>
         <Link to="/shop" className="option">
-					CONTACT
+          CONTACT
 				</Link>
+        { 
+          currentUser ? 
+            <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>
+            :
+            <Link className='option' to='/signin'>SIGN IN</Link>
+        }
 			</div>
 		</div>
 	);
