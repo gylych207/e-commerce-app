5f5ce07a8260f170630af279db6cfb1e046e1ae3
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-07 17:51:32 -0500

    sign up component added to the signIn-signUp page

--- src/components/signInSignUp/SignInSignUp.jsx
+++ src/components/signInSignUp/SignInSignUp.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignIn from '../signIn/SignIn';
+import SignUp from '../signUp/SignUp';
+
 import './styles.scss';
 
 const SignInSignUp = () => {
   return (
     <div className='sign-in-and-sign-up'>
-      <SignIn/>
+      <SignIn />
+      <SignUp />
     </div>
   );
 };
