358642273315bc6bd9d3075e7208b62966bb0fee
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-11 19:29:16 -0500

    cart action types and reducer

--- src/components/cardIcon/CardIcon.jsx
+++ src/components/cardIcon/CardIcon.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import {toggleCartHidden} from '../../redux/cart/cart-actions.js'
 import './styles.scss';
 
 const CardIcon = () => {
