d7fa5127484a6483b84ff7170a1c04941335deb6
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-03-12 20:50:58 -0500

    collection item updated

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -5,21 +5,28 @@ import {addItem} from '../../redux/cart/cart-actions'
  
 import './styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl,addItem }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-    <CustomButton inverted>Add to Cart</CustomButton>
-  </div>
-);
+  );
+};
+
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
 });
